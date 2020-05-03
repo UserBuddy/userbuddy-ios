33a0bc81cb1bcb424a18e116f67fa398212a8d34
ryanbliss <smile@ryanbliss.me>
2020-05-02 18:32:54 -0600

    Initial Commit

---
+++ UserBuddy/UserBuddy.h
@@ -0,0 +1,19 @@
+//
+//  UserBuddy.h
+//  UserBuddy
+//
+//  Created by Ryan Bliss on 5/2/20.
+//  Copyright © 2020 UserBuddy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UserBuddy.
+FOUNDATION_EXPORT double UserBuddyVersionNumber;
+
+//! Project version string for UserBuddy.
+FOUNDATION_EXPORT const unsigned char UserBuddyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UserBuddy/PublicHeader.h>
+
+
